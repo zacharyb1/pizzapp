e893e869041a6de930f92334b829679dfe2075e8
zacharyb1 <zachary.burda@gmail.com>
2025-08-28 13:40:34 +0300

    Integrate Firebase services by adding necessary configuration files and dependencies for both Android and iOS platforms; update build scripts and app view imports.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,15 @@
+buildscript {
+    val kotlinVersion = "1.7.10"
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("com.google.gms:google-services:4.3.15")
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -5,16 +17,14 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
+val newBuildDir = rootProject.layout.buildDirectory.dir("../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    val newSubprojectBuildDir = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+/*import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pizza_app/blocs/authentication_bloc/authentication_bloc.dart';
 import 'package:pizza_app/screens/auth/blocs/sing_in_bloc/sign_in_bloc.dart';
 import 'package:pizza_app/screens/home/blocs/get_pizza_bloc/get_pizza_bloc.dart';
 import 'package:pizza_repository/pizza_repository.dart';
 
 import 'screens/auth/views/welcome_screen.dart';
-import 'screens/home/views/home_screen.dart';
+import 'screens/home/views/home_screen.dart';*/
 
 class MyAppView extends StatelessWidget {
   const MyAppView({super.key});
@@ -15,7 +15,7 @@ class MyAppView extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Pizza Delivery',
-        debugShowCheckedModeBanner: false,
+        /*debugShowCheckedModeBanner: false,
         theme: ThemeData(colorScheme: ColorScheme.light(background: Colors.grey.shade200, onBackground: Colors.black, primary: Colors.blue, onPrimary: Colors.white)),
         home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
           builder: ((context, state) {
@@ -37,6 +37,7 @@ class MyAppView extends StatelessWidget {
               return const WelcomeScreen();
             }
           }),
-        ));
+        )*/
+        );
   }
 }
